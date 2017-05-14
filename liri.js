55d28b28f0d06e419cb629b9e498cf09d93d67ca
Seth Gibson <sethg@seths-mbp.attlocal.net>
2017-05-14 08:46:23 -0500

    modularized twitter

--- liri.js
+++ liri.js
@@ -1,26 +1,5 @@
-var keys = require('./keys');
+var getTweets = require('./twitter');
 
 console.log('\n\n Welcome to Liri! The lame version of Siri.\n\n');
 
-// API resources
-var Twitter = require('twitter');
-
-var client = new Twitter({
-  consumer_key: keys.twitterKeys.consumer_key,
-  consumer_secret: keys.twitterKeys.consumer_secret,
-  access_token_key: keys.twitterKeys.access_token_key,
-  access_token_secret: keys.twitterKeys.access_token_secret
-});
-
-var params = {screen_name: 'sethg4thdown'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-
-    tweets.forEach(function(tweet) {
-        console.log(tweet.created_at);
-        console.log(tweet.text);
-    });
-  } else {
-      console.log(error);
-  }
-});
+getTweets();
---
+++ twitter.js
@@ -0,0 +1,27 @@
+var keys = require('./keys');
+// API resources
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+var params = {screen_name: 'sethg4thdown'};
+
+function getTweets() {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            tweets.forEach(function(tweet) {
+                console.log(tweet.created_at);
+                console.log(tweet.text);
+            });
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+module.exports = getTweets;
