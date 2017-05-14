1a934c916598245c6d5bcc1b4f7b06c02f4bd19f
Seth Gibson <sethg@seths-mbp.attlocal.net>
2017-05-14 08:17:24 -0500

    added twitter config

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'qrCJHPcSTGWLXv7jupNRDYeMz',
+  consumer_secret: 'yqFDzd7ZHjaGVVv2OxpG6d5SQrvCyOb2H8vaS2bVUSzUwcPYaO',
+  access_token_key: '863743084106047492-iXeYyUHiGrUkOkynWtp8AcyEwKS3OQV',
+  access_token_secret: 'gpTRPMHR3gP3eAu5xnBc2bWXmJsRjThjE7dJY1Zdm2YSu',
+}
